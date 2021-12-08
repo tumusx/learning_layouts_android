8bad8dd999577fc9eceb84bf4e6986f0333ad7bd
Murillo Alves <tumusx1@gmail.com>
2021-12-07 22:39:57 -0300

    create layout project

---
+++ app/src/main/java/murillo/silva/layouts/MainActivity.kt
@@ -0,0 +1,11 @@
+package murillo.silva.layouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ex_largura_altura)
+    }
+}
